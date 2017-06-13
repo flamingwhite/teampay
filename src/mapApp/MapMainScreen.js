f39f5c00e48e3de399d17282a127d0141c2efcdd
Yong Wang <389816@ac02t11u8gyfh.genpt.net>
2017-06-13 13:24:55 -0400

    map

--- src/mapApp/MapMainScreen.js
+++ src/mapApp/MapMainScreen.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import { Container, Header, Text, Title, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+import { StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
 
 console.log(MapView);
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
 
 export default class MapMainScreen extends Component {
 	render() {
@@ -11,9 +31,9 @@ export default class MapMainScreen extends Component {
 				<Header>
 					<Title>Header</Title>
 				</Header>
-				<Content>
+				<Content style={styles.container}>
 					<Text>What's up Map</Text>
-					<MapView initialRegion={ { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421, } } style={{flex:1, height:300, width:300}}/>
+					<MapView style={styles.map} initialRegion={ { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421, } } style={{flex:1, height:300, width:300}}/>
 				</Content>
 				<Footer>
 					<FooterTab>
